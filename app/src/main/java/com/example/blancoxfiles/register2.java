cd62d6b5afde07b6941517f9d20e022c8fee5044
HidalgoFernando <fh017049@gmail.com>
2023-11-02 18:53:05 -0300

    pe

--- app/src/main/java/com/example/blancoxfiles/register2.java
+++ app/src/main/java/com/example/blancoxfiles/register2.java
@@ -11,9 +11,9 @@ import android.widget.TextView;
 
 public class register2 extends AppCompatActivity {
 
-    EditText ConfirmarContrasena, Nombre, Apellido, Cumple, ;
-    TextView mensaje;
-    ImageView Logo;
+    EditText ConfirmarContra, Contra, Nombre, Apellido, Cumpleanos;
+    TextView Consigna;
+    ImageView Imagen;
     RadioButton Mujer, Hombre;
     Button Boton;
     @Override
@@ -22,6 +22,17 @@ public class register2 extends AppCompatActivity {
         setContentView(R.layout.activity_register2);
 
         Boton = findViewById(R.id.Registrar);
+        Mujer = findViewById(R.id.Mujer1);
+        Hombre = findViewById(R.id.Hombre1);
+        Consigna = findViewById(R.id.mensaje);
+        Imagen = findViewById(R.id.Logo);
+        Nombre = findViewById(R.id.Nombre1);
+        Apellido = findViewById(R.id.Apellido1);
+        Cumpleanos = findViewById(R.id.Cumple);
+        Contra = findViewById(R.id.Contrasena);
+        ConfirmarContra = findViewById(R.id.ConfirmContrasena);
+
+
 
     }
 }
