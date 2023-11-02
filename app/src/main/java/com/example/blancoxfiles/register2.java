33d553a05c0f12a430f3c0b389d6911a3776e5b6
HidalgoFernando <fh017049@gmail.com>
2023-11-02 18:44:38 -0300

    pe

--- app/src/main/java/com/example/blancoxfiles/register2.java
+++ app/src/main/java/com/example/blancoxfiles/register2.java
@@ -3,12 +3,25 @@ package com.example.blancoxfiles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class register2 extends AppCompatActivity {
 
+    EditText ConfirmarContrasena, Nombre, Apellido, Cumple, ;
+    TextView mensaje;
+    ImageView Logo;
+    RadioButton Mujer, Hombre;
+    Button Boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        Boton = findViewById(R.id.Registrar);
+
     }
 }
