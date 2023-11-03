1c06818da302630399a56f65d6fe06d4dc8c7ae1
HidalgoFernando <fh017049@gmail.com>
2023-11-02 21:48:13 -0300

    pe

---
+++ app/src/main/java/com/example/blancoxfiles/miperfil.java
@@ -0,0 +1,14 @@
+package com.example.blancoxfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class miperfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miperfil);
+    }
+}
